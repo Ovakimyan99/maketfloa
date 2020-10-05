86ddd4607451e3a333c93ace3593548985c6e0b0
Тигран <tigr.kman@gmail.com>
2020-10-05 12:59:10 +0300

    На этапе разработки мобильной адаптации

---
+++ scripts/wis/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  $(".slider").owlCarousel({
+    items: 1,
+    dots: true,
+    dotsClass: "slider-dots",
+    nav: false,
+    loop: false,
+    autoplay: false,
+    center: false
+  });
+});
+
+
+// const 
