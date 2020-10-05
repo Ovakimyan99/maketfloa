ce3d91d1e20aa17a1db1c5c1ce27837f5a922183
Тигран <tigr.kman@gmail.com>
2020-10-05 15:25:53 +0300

    Разработал меню для телефонов, начал делать адаптив под них же

--- scripts/wis/main.js
+++ scripts/wis/main.js
@@ -1,3 +1,5 @@
+'use strict';
+
 $(document).ready(function() {
   $(".slider").owlCarousel({
     items: 1,
@@ -11,4 +13,68 @@ $(document).ready(function() {
 });
 
 
-// const 
+var Menu = {
+  
+  el: {
+    ham: $('.header-burger'),
+    menuTop: $('.header-burger__top'),
+    menuMiddle: $('.header-burger__middle'),
+    menuBottom: $('.header-burger__bottom'),
+    mobileNav: $('.mobile-nav')
+  },
+  
+  init: function() {
+    Menu.bindUIactions();
+  },
+  
+  bindUIactions: function() {
+    Menu.el.ham
+        .on(
+          'click',
+        function(event) {
+        Menu.activateMenu(event);
+        event.preventDefault();
+        
+      }
+    );
+  },
+  
+  activateMenu: function() {
+    Menu.el.menuTop.toggleClass('header-burger__top-click');
+    Menu.el.menuMiddle.toggleClass('header-burger__middle-click');
+    Menu.el.menuBottom.toggleClass('header-burger__bottom-click');
+    Menu.el.mobileNav.toggleClass('mobile-nav--active');
+  }
+};
+
+Menu.init();
+
+const headerDrop = document.querySelector('.header-wis__drop'),
+      headerBurger = document.querySelector('.header-burger');
+
+const overflow = document.createElement('div');
+overflow.classList.add('overflow');
+
+headerBurger.addEventListener('click', ()=>{
+if(document.querySelector('.header-burger__top-click')){
+  openMenu();
+} else {
+  closeMenu();
+}
+});
+
+overflow.addEventListener('click', ()=>{
+closeMenu();
+});
+
+function openMenu() {
+headerDrop.style.height = '19rem';
+document.body.style.overflow = 'hidden';
+document.body.append(overflow);
+}
+
+function closeMenu() {
+headerDrop.style.height = '0';
+document.body.style.overflow = '';
+document.body.querySelector('.overflow').remove();
+}
